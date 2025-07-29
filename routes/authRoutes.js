7ccd1e82f8e087184bc01dcf4c961de004fead7e
Apple <apple@Dhruvs-MAC2.local>
2025-07-29 23:50:07 +0530

    first commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 // Handle preflight requests for all routes in this router
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000","https://zynetic-frontend-git-main-dhruv2183s-projects.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
